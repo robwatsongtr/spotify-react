e97c22f148aab2d56c20749fe3c61f39121f73bd
robwatsongtr <rwatso@gmail.com>
2021-10-28 16:56:17 -0700

    working on server

--- server/index.js
+++ server/index.js
@@ -6,10 +6,25 @@ const port = 5000
 
 dotenv.config()
 
-var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
-var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
+const spotify_client_id = process.env.SPOTIFY_CLIENT_ID
+const spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
 
-var app = express();
+// randomly generated string to protect against attacks such as cross-site forgery
+const generateRandomString = (length) => {
+
+  let text = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  
+  for(let i = 0; i < length; i++) {
+    text += possible.charAt( Math.floor( Math.random() * possible.length) )
+  }
+  return text 
+
+}
+
+
+
+const app = express();
 
 app.get('/auth/login', (req, res) => {
 });
---
+++ server/randomStringGenerator.js
@@ -0,0 +1,13 @@
+const generateRandomString = (length) => {
+
+  let text = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  
+  for(let i = 0; i < length; i++) {
+    text += possible.charAt( Math.floor( Math.random() * possible.length) )
+  }
+  return text 
+
+}
+
+console.log( generateRandomString(44) )
