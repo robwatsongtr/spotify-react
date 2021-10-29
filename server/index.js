2b353f5c384782b0a8dd510f4f0f34a98540e18a
robwatsongtr <rwatso@gmail.com>
2021-10-28 18:30:12 -0700

    working on auth endpoint on server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 
 const express = require('express')
 const dotenv = require('dotenv');
-
+const app = express();
 const port = 5000
 
 dotenv.config()
 
 const spotify_client_id = process.env.SPOTIFY_CLIENT_ID
 const spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
+const spotify_redirect_uri = 'http://localhost:3000/auth/callback'
 
 // randomly generated string to protect against attacks such as cross-site forgery
 const generateRandomString = (length) => {
@@ -22,15 +23,36 @@ const generateRandomString = (length) => {
 
 }
 
-
-
-const app = express();
-
+// redirect to Spotify Login screen 
 app.get('/auth/login', (req, res) => {
+
+  let scope = 
+    "streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state";
+  
+  // randomly generated string to protect against attacks 
+  let state = generateRandomString(16);
+
+  let auth_query_parameters = new URLSearchParams({
+
+    response_type: "code",
+    client_id: spotify_client_id,
+    scope: scope,
+    redirect_uri: spotify_redirect_uri,
+    state: state
+
+  })
+
+  res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
+  
 });
+
 
 app.get('/auth/callback', (req, res) => {
+
+  
+  
 });
+
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
