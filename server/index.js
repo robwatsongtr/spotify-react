c995f72b7954ca929f888fc141d7129b75f8e6b4
robwatsongtr <rwatso@gmail.com>
2021-10-29 14:28:59 -0700

    added comments on server code

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 const app = express();
 const port = 5000
 
+// Rather than hard-coding the user credentials inside the source code of our application, 
+//we are going to use the dotenv package to store and read them from a hidden configuration file
 dotenv.config()
 
 const spotify_client_id = process.env.SPOTIFY_CLIENT_ID
@@ -24,12 +26,33 @@ const generateRandomString = (length) => {
 }
 
 // redirect to Spotify Login screen 
+
+// To do so, we need to send a GET request to the /authorize endpoint of the Spotify account 
+// service with the following parameters:
+
+// response_type 
+// is the credential that will be returned. The value will always be code.
+
+// client_id, 
+// is the Client ID of the application we have just created on the portal dashboard.
+
+// scope
+// a space-separated list of actions that our app can be allowed to do on a user’s behalf. 
+// We need permission for streaming, user-read-email and user-read-private for the Web Player SDK.
+
+// redirect_uri 
+// is the URL that Spotify’s Authorization server will redirect once the access token 
+// is granted. Since we are running the project locally, our redirect URL will point to 
+// localhost:3000/auth/callback since all petitions are handled from the frontend.
+
+// state 
+// a randomly generated string to protect against attacks such as cross-site request forgery.
+
 app.get('/auth/login', (req, res) => {
 
   let scope = 
     "streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state";
   
-  // randomly generated string to protect against attacks 
   let state = generateRandomString(16);
 
   let auth_query_parameters = new URLSearchParams({
@@ -47,11 +70,19 @@ app.get('/auth/login', (req, res) => {
 });
 
 
+
+
 app.get('/auth/callback', (req, res) => {
+
 
   
   
 });
+
+
+
+
+
 
 
 app.listen(port, () => {
